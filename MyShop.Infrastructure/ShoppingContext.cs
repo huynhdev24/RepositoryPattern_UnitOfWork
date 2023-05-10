4035d04c9d6a9303133b7d8807078728d05281e5
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-10 15:22:18 +0700

    implement repository in Infrastructure

---
+++ MyShop.Infrastructure/ShoppingContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Infrastructure
+{
+    public class ShoppingContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                // .UseLazyLoadingProxies()
+                .UseSqlite("Data Source=orders.db");
+        }
+    }
+}
