721ab447e0290ed10fcb6647059e24a4c55269fa
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-10 15:51:44 +0700

    implement CustomerController and OrderController

---
+++ MyShop.Web/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.Domain.Models;
+using MyShop.Infrastructure.Repositories;
+
+namespace MyShop.Web.Controllers
+{
+    [Route("api/customer")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IRepository<Customer> repository;
+
+        public CustomerController(IRepository<Customer> repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Index(Guid? id)
+        {
+            if (id == null)
+            {
+                var customers = repository.All();
+
+                return Ok(customers);
+            }
+            else
+            {
+                var customer = repository.Get(id.Value);
+
+                return Ok(new[] { customer });
+            }
+        }
+    }
+}
